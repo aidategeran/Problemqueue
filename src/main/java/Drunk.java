87c64d9780e6b7e5ebc0647eac508200f0de98b2
AidaTegeran <aidategeran@gmail.com>
2025-05-07 22:02:22 -0500

    commit

--- src/main/java/Drunk.java
+++ src/main/java/Drunk.java
@@ -83,6 +83,22 @@ public class Drunk {
     public int nextInt() {
         return Integer.parseInt(next());
     }
+
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
 }
 
 
